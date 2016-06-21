c182d71745110a74e053e324beb20b692edea5a2
Tom Dietrich <tom.dietrich.contractor@8451.com>
2016-06-21 11:13:12 -0400

    removed consumer, refactored to lambdas

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ var appEnv = require('cfenv').getAppEnv({
     services: {
       'p-rabbitmq': [
         {
-          name: 'RMQServiceBus',
-          uri: 'amqp://172.17.0.2:5672'
+          credentials: {
+            name: 'RMQServiceBus',
+            uri: 'amqp://172.17.0.2:5672'
+          }
         }
       ]
     }
@@ -21,48 +23,28 @@ var appEnv = require('cfenv').getAppEnv({
 
 var port = appEnv.port || 8081;
 var api = require('amqplib/callback_api');
-var rmquri = appEnv.getService('RMQServiceBus').uri;
-var q = 'tasks';
-
+var rmquri = appEnv.getService('RMQServiceBus').credentials.uri;
+console.log(`attaching to rabbit at uri ${rmquri}`);
 var textParser = bodyParser.text();
 
 function bail (err, res) {
   res.status(500).send(err);
 }
 
-app.post('/exchange/:exc/route/:rk', textParser, function(req, res) {
+app.post('/exchange/:exc/route/:rk', textParser, (req, res) => {
   let exchange = req.params.exc;
   let routeKey = req.params.rk;
-  api.connect(rmquri, function(err, conn) {
-    conn.createChannel(on_open);
-    function on_open(err, ch) {
+  api.connect(rmquri, (err, conn) => {
+    conn.createChannel((err, ch) => {
       if (err != null) bail(err, res);
       ch.assertExchange(exchange);
       ch.publish(exchange, routeKey, new Buffer(req.body));
       res.sendStatus(200);
       console.log(`published message to ${exchange} using routing key ${routeKey}`);
       ch.close();
-    }
+    });    
   });
 });
-
-function consumer(conn) {
-  var ok = conn.createChannel(on_open);
-  function on_open( err, ch) {
-    if (err != null) bail (err);
-    ch.assertQueue(q);
-    ch.consume(q, function(msg) {
-      if (msg !== null) {
-        console.log(msg.content.toString());
-        ch.ack(msg);
-      }
-    });
-  }
-}
-
-api.connect(rmquri, function(err, conn) {
-    consumer(conn);
-  });
 
 app.listen(port);
 console.log(`listening on ${port}`);
